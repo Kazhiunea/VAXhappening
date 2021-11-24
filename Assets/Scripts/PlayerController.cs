561d5fdc4676b0d305f9dae3f85f1c2ae6ecd8b1
Kazhiunea <inc.aleks@gmail.com>
2021-11-24 17:18:09 +0200

    Basic animations and sprites + player movement
    Title

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float Speed;
     private Rigidbody2D body;
+    private Animator animator;
+    private bool grounded = true;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,20 +23,35 @@ public class PlayerController : MonoBehaviour
 
         if(horizontalInput > 0.01f)
         {
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(0.05f, 0.05f, 1);
         }else if(horizontalInput < -0.01f)
         {
-            transform.localScale = new Vector3(-1, 1 ,1);
+            transform.localScale = new Vector3(-0.05f, 0.05f ,1);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && grounded == true)
         {
-            body.velocity = new Vector2(body.velocity.x, Speed);
+            Jump();
+            
         }
 
-
-
+        //Seting the animation when running
+        animator.SetBool("Running", horizontalInput != 0);
+        animator.SetBool("Grounded", grounded);
     }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, Speed*3);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
 
 }
